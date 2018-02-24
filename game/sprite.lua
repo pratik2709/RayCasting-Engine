98c82f0a0649c5fc0ec88c6e07d61be5ce905079
pratiksaxena <pratik2709@gmail.com>
2018-02-24 12:36:22 +0530

    deconstructing the loop

--- game/sprite.lua
+++ game/sprite.lua
@@ -41,8 +41,8 @@ function drawSprites101(distArray)
 
         local left = screenMiddle - size / 2 + x
 
-        local top = (screenHeight/2 - (0.55) * size)
-        local sy = (64 * 0.01 * size) + (0.45)*size
+        local top = (screenHeight / 2 - (0.55) * size)
+        local sy = (64 * 0.01 * size) + (0.45) * size
 
         local dbx = sprite.x - player.x
         local dby = sprite.y - player.y
@@ -57,23 +57,54 @@ function drawSprites101(distArray)
         if not (size <= 0) then
             local cumulativeDS = 0
             local cumulativeTS = 0
+            local tx = 0
             -- draw sprite strip by strip
-            local strips = size / stripWidth
-            local q
---            print("***")
-            for i = 0, size, 1
-            do
-                cumulativeDS = (cumulativeDS + (stripWidth))
-                love.graphics.setColor(255, 255, 255)
-                q = love.graphics.newQuad(cumulativeDS/size, 0, 64, 64, armorImage:getDimensions())
-                love.graphics.draw(armorImage, q, left + cumulativeDS/size, top, 0, size/64, size / 64)
-            end
---            print("***")
+            local strips = math.floor(size / stripWidth)
+--            print(strips)
+            --            print("***")
+--            for i = 0, strips, 1
+--            do
+--                cumulativeDS = (cumulativeDS + (stripWidth))
+--                cumulativeTS = math.floor(cumulativeDS * sprite.spriteWidth / size)
+--                if cumulativeTS > sprite.spriteWidth then
+--                    cumulativeTS = sprite.spriteWidth
+--                end
+--
+--                love.graphics.setColor(255, 255, 255)
+--                left = left + cumulativeTS
+--                local q = love.graphics.newQuad(cumulativeDS, 0, stripWidth, 64, armorImage:getDimensions())
+--                love.graphics.draw(armorImage, q, left, top, 0, size / 64, size / 64)
+--                cumulativeTS = 0
+--                cumulativeDS = 0
+--            end
+            --            print("***")
 
             -- draw the sprite in 1 go
---                        love.graphics.setColor(255,255,255)
---                        local q = love.graphics.newQuad( 0, 0, 64, 64, armorImage:getDimensions())
---                        love.graphics.draw(armorImage, q, left, top ,0, size/64, size/64)
+
+            love.graphics.setColor(255, 255, 255)
+            local q = love.graphics.newQuad(16, 0, 8, 64, armorImage:getDimensions())
+            left = left + size
+            love.graphics.draw(armorImage, q, left, top, 0, size / 64, size / 64)
+            print("***")
+            print(left)
+            love.graphics.setColor(255, 255, 255)
+            local q = love.graphics.newQuad(16, 0, 8, 64, armorImage:getDimensions())
+            left = left + size
+            love.graphics.draw(armorImage, q, left, top, 0, size / 64, size / 64)
+            print(left)
+            print("***")
+
+--            love.graphics.setColor(255, 255, 255)
+--            local q = love.graphics.newQuad(16, 0, 8, 64, armorImage:getDimensions())
+--            love.graphics.draw(armorImage, q, left+24, top, 0, size / 64, size / 64)
+--
+--            love.graphics.setColor(255, 255, 255)
+--            local q = love.graphics.newQuad(16, 0, 8, 64, armorImage:getDimensions())
+--            love.graphics.draw(armorImage, q, left + 32, top, 0, size / 64, size / 64)
+--
+--            love.graphics.setColor(255, 255, 255)
+--            local q = love.graphics.newQuad(16, 0, 8, 64, armorImage:getDimensions())
+--            love.graphics.draw(armorImage, q, left + 40, top, 0, size / 64, size / 64)
         end
     end
 end
